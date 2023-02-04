c78e8613d55e921e5e886d7b0626fc3d3b281d92
fep <ferpomu@gmail.com>
2023-02-04 01:11:11 -0600

    adding header for files

--- fp_sqr.v
+++ fp_sqr.v
@@ -1,3 +1,17 @@
+/* Copyright 2023 Fereie
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 // fp multiplication of 2 32 bit fp numbers
 //pipelined fp multiplier, supports 2 of the 2 rounding modes truncate and round to nearest (even on tie)
 // fp multiplication of 2 32 bit fp numbers
--- special_characters.v
+++ special_characters.v
@@ -1,3 +1,17 @@
+/* Copyright 2023 Fereie
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 `define FP_ZEROP {1'b0,8'h00,23'b0} // positive zero
 `define FP_ZERON {1'b1,8'h00,23'b0} // negative zero
 `define FP_INFP {1'b0,8'hFF,23'b0} //positive infinity 
